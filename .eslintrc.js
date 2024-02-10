a5db0a280958c4c1708dbf092b74161d6bd3c8a2
John santhosh <JSanthosh@caesars.com>
2024-02-10 17:55:32 +0530

    initial server sertup

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+//   overrides: [
+//     {
+//       env: {
+//         node: true,
+//       },
+//       files: ['.eslintrc.{js,cjs}'],
+//       parserOptions: {
+//         sourceType: 'script',
+//       },
+//     },
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//   },
+//   plugins: ['@typescript-eslint'],
+//   rules: {},
+// };
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
