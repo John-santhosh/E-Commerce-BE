1d8450e8ed7b2864cc7f73ca367515fba8325465
John santhosh <JSanthosh@caesars.com>
2024-03-09 17:53:22 +0530

    login endpoint done

--- src/controllers/userAuthController.ts
+++ src/controllers/userAuthController.ts
@@ -1,25 +1,61 @@
 import { Request, Response } from "express";
 import { sendResponse } from "@helpers";
+import { prisma } from "src/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Prisma } from "@prisma/client";
 
 export class UserAuthController {
   //p
   login = async (request: Request, response: Response) => {
-    return response.send(200);
+    const { email, password } = request?.body;
+    if (!email) return sendResponse(404, { error: "Email Required" }, response);
+    if (!password) return sendResponse(404, { error: "Password Required" }, response);
+
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    if (!user) return sendResponse(401, { error: "User not found" }, response);
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) return sendResponse(401, { error: "Wrong Password" }, response);
+
+    // const token =
+    const secretKey = process.env.ACCESS_TOKEN_SECRET;
+
+    const authToken = jwt.sign({ email, password }, secretKey || "", { expiresIn: "100s" });
+    // console.log({ authToken });
+
+    const refToken = jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET || "", { expiresIn: "3d" });
+
+    await prisma.users.update({
+      where: { email },
+      data: {
+        refreshToken: refToken,
+      },
+    });
+    await prisma.user_sessions.create({
+      data: {
+        authToken: authToken,
+        user_id: user.id,
+      },
+    });
+    return response.status(200).send({ email, password, authToken });
   };
   //p
   logOut = async (request: Request, response: Response) => {
-    return response.send(200);
+    return response.sendStatus(200);
   };
   // p
   forgetPassword = async (request: Request, response: Response) => {
-    return response.send(200);
+    return response.sendStatus(200);
   };
   // p
   emailVerification = async (request: Request, response: Response) => {
-    return response.send(200);
+    return response.sendStatus(200);
   };
   //g
   verifyOTP = async (request: Request, response: Response) => {
-    return response.send(200);
+    return response.sendStatus(200);
   };
 }
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../server";
 import { sendResponse } from "@helpers";
+import bcrypt from "bcrypt";
 
 interface Body {
   firstName: string;
@@ -20,20 +21,24 @@ export class UserController {
 
     try {
       const foundUser = await prisma.users.findUnique({ where: { email } });
-      console.log({ foundUser });
+      // console.log({ foundUser });
 
       if (foundUser) return sendResponse(400, { error: "Email Already taken" }, response);
+      const secretPasswd = await bcrypt.hash(password, 10);
 
       const res = await prisma.users.create({
-        data: { ...request.body },
+        data: { ...request.body, password: secretPasswd } as Body,
       });
 
       if (res.id) return sendResponse(200, { success: "userCreated" }, response);
 
       return sendResponse(503, { error: "Service not available" }, response);
     } catch (error: any) {
-      console.log("error creating user");
+      console.log("error creating user", error.message);
       sendResponse(500, { error: "Internal server error" }, response);
     }
   };
+
+  // can be accessible only when logged in.
+  updateUser = async (request: Request, response: Response) => {};
 }
